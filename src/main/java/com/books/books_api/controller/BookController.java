51c5adb3d825c22365170f3911d7494e2064f618
Murphy Potts <mpotts0201@gmail.com>
2026-09-15 15:39:09 -0400

    Now using service in controller, removed hardocded response

--- src/main/java/com/books/books_api/controller/BookController.java
+++ src/main/java/com/books/books_api/controller/BookController.java
@@ -1,6 +1,8 @@
 package com.books.books_api.controller;
 
 import com.books.books_api.dto.BookDto;
+import com.books.books_api.service.BookService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +11,13 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/books")
+@RequiredArgsConstructor
 public class BookController {
+
+    private final BookService bookService;
 
     @GetMapping
     public List<BookDto> getBooks() {
-        return List.of(
-                new BookDto(1L, "The Hobbit", "J.R.R. Tolkien", 1937),
-                new BookDto(2L, "Dune", "Frank Herbert", 1965)
-        );
+        return bookService.getAllBooks();
     }
 }
